05c9efdf6f37941a8ac0009a9b71f6a32d5c8a49
Smol Noob <>
2021-05-15 19:58:39 +0000

    enter works!!!!

--- main.js
+++ main.js
@@ -8,49 +8,58 @@ var text = 15;
 document.getElementById("messages").disabled = true;
 
 var isHosting = function () {
-    console.log(document.getElementById('lname').value, "is hosting");
-    var peer = new Peer(document.getElementById('lname').value);
-    peer.on('connection', function (conn) {
-        document.getElementById("messages").disabled = false;
-        myConn = conn;
-        conn.on('data', function (data) {
-          ctx.font = "20px Arial";
-          ctx.fillStyle = "#808080";
-          ctx.fillText(data, 300, text);
-          text += 20; 
-        });
+  console.log(document.getElementById('lname').value, "is hosting");
+  var peer = new Peer(document.getElementById('lname').value);
+  peer.on('connection', function (conn) {
+    document.getElementById("messages").disabled = false;
+    myConn = conn;
+    conn.on('data', function (data) {
+      ctx.font = "20px Arial";
+      ctx.fillStyle = "#808080";
+      ctx.fillText(data, 300, text);
+      text += 20;
     });
+  });
 };
 hostingButton.addEventListener("click", isHosting);
 
 var joiningButton = document.getElementById("joining");
 var isJoining = function () {
-    console.log(document.getElementById('lname').value, "is joining");
+  console.log(document.getElementById('lname').value, "is joining");
 
-    var peer = new Peer();
-    peer.on("open", function () {
-        var conn = peer.connect(document.getElementById('lname').value); conn.on('open', function () {
-            document.getElementById("messages").disabled = false;
-            myConn = conn;
-            myConn.on('data', function (data) {
-              ctx.font = "20px Arial";
-              ctx.fillStyle = "#808080";
-              ctx.fillText(data, 300, text);
-              text += 20;
-            })
-        })
-    });
+  var peer = new Peer();
+  peer.on("open", function () {
+    var conn = peer.connect(document.getElementById('lname').value); conn.on('open', function () {
+      document.getElementById("messages").disabled = false;
+      myConn = conn;
+      myConn.on('data', function (data) {
+        ctx.font = "20px Arial";
+        ctx.fillStyle = "#808080";
+        ctx.fillText(data, 300, text);
+        text += 20;
+      })
+    })
+  });
 };
 joiningButton.addEventListener("click", isJoining);
 
 var chattingButton = document.getElementById("messages");
 var chatting = function () {
-    myConn.send(document.getElementById('message').value);
-    ctx.font = "20px Arial";
-    ctx.fillStyle = "#0000ff";
-    ctx.fillText(document.getElementById('message').value, 5, text);
-    text += 20;
+  myConn.send(document.getElementById('message').value);
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "#0000ff";
+  ctx.fillText(document.getElementById('message').value, 5, text);
+  text += 20;
 };
+
+
+var input = document.getElementById("message");
+
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("messages").click();
+  }
+});
+
 chattingButton.addEventListener("click", chatting);
-
-
