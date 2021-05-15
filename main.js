4f96ca9d9ec9caee98a3e3be3e69d509e5709f39
Smol Noob <>
2021-05-15 05:41:43 +0000

    greyed out before the connection is establashed

--- main.js
+++ main.js
@@ -5,10 +5,13 @@ var myConn = null;
 
 var text = 15;
 
+document.getElementById("messages").disabled = true;
+
 var isHosting = function () {
     console.log(document.getElementById('lname').value, "is hosting");
     var peer = new Peer(document.getElementById('lname').value);
     peer.on('connection', function (conn) {
+        document.getElementById("messages").disabled = false;
         myConn = conn;
         conn.on('data', function (data) {
           ctx.font = "20px Arial";
@@ -27,6 +30,7 @@ var isJoining = function () {
     var peer = new Peer();
     peer.on("open", function () {
         var conn = peer.connect(document.getElementById('lname').value); conn.on('open', function () {
+            document.getElementById("messages").disabled = false;
             myConn = conn;
             myConn.on('data', function (data) {
               ctx.font = "20px Arial";
@@ -48,4 +52,5 @@ var chatting = function () {
     text += 20;
 };
 chattingButton.addEventListener("click", chatting);
+
 
