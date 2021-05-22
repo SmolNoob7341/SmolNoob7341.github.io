9a32040b8fa7ccb08fde1bac59cc6bdf17c391a0
Smol Noob <>
2021-05-22 03:20:37 +0000

    ME and HIM added

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 var hostingButton = document.getElementById("hosting");
 var myConn = null;
 
-
 document.getElementById("messages").disabled = true;
 
+var previousMessageAuthor = "";
 var isHosting = function () {
   console.log(document.getElementById('lname').value, "is hosting");
   var peer = new Peer(document.getElementById('lname').value);
   peer.on('connection', function (conn) {
     document.getElementById("messages").disabled = false;
     myConn = conn;
-      myConn.on('data', function (data) {
+    myConn.on('data', function (data) {
+      if (previousMessageAuthor == "me" || previousMessageAuthor == "") {
         var iDiv = document.createElement('div');
         iDiv.id = 'block';
-        iDiv.className = 'messageIn';
-        iDiv.innerHTML = data;
+        iDiv.className = 'him';
+        iDiv.innerHTML = "HIM";
         document.getElementById('border').appendChild(iDiv);
+      }
+      var iDiv = document.createElement('div');
+      iDiv.id = 'block';
+      iDiv.className = 'messageIn';
+      iDiv.innerHTML = data;
+      document.getElementById('border').appendChild(iDiv);        previousMessageAuthor = "him";
     });
   });
 };
@@ -24,18 +31,25 @@ hostingButton.addEventListener("click", isHosting);
 var joiningButton = document.getElementById("joining");
 var isJoining = function () {
   console.log(document.getElementById('lname').value, "is joining");
-
   var peer = new Peer();
   peer.on("open", function () {
     var conn = peer.connect(document.getElementById('lname').value); conn.on('open', function () {
       document.getElementById("messages").disabled = false;
       myConn = conn;
       myConn.on('data', function (data) {
+        if (previousMessageAuthor == "me" || previousMessageAuthor == "") {
+          var iDiv = document.createElement('div');
+          iDiv.id = 'block';
+          iDiv.className = 'him';
+          iDiv.innerHTML = "HIM";
+          document.getElementById('border').appendChild(iDiv);
+        }
         var iDiv = document.createElement('div');
         iDiv.id = 'block';
         iDiv.className = 'messageIn';
         iDiv.innerHTML = data;
         document.getElementById('border').appendChild(iDiv);
+        previousMessageAuthor = "him";
       })
     })
   });
@@ -44,23 +58,23 @@ joiningButton.addEventListener("click", isJoining);
 
 var chattingButton = document.getElementById("messages");
 var chatting = function () {
-  myConn.send(document.getElementById('message').value);
-  if(document.getElementById('message').value == "Hi"){
+   previousMessageAuthor
+  if (previousMessageAuthor == "him" || previousMessageAuthor == "") {
     var iDiv = document.createElement('div');
     iDiv.id = 'block';
-    iDiv.className = 'YOU';
-    iDiv.innerHTML = "You";
+    iDiv.className = 'me';
+    iDiv.innerHTML = "ME";
     document.getElementById('border').appendChild(iDiv);
-    document.getElementById('message').value = '';
   }
+  myConn.send(document.getElementById('message').value);
   var iDiv = document.createElement('div');
   iDiv.id = 'block';
   iDiv.className = 'messageOut';
   iDiv.innerHTML = document.getElementById('message').value;
   document.getElementById('border').appendChild(iDiv);
   document.getElementById('message').value = '';
+  previousMessageAuthor = "me";
 };
-
 
 var input = document.getElementById("message");
 
@@ -69,7 +83,6 @@ input.addEventListener("keyup", function (event) {
     event.preventDefault();
     document.getElementById("messages").click();
   }
-});
-
+})
 chattingButton.addEventListener("click", chatting);
 
