e3bb159ee10d8f117414c560faf937fbbfd74545
Smol Noob <>
2021-06-08 04:43:02 +0000

    finished version 2.0

--- main.js
+++ main.js
@@ -1,85 +1,142 @@
-import http from 'https://unpkg.com/isomorphic-git@beta/http/web/index.js'
+var handleMessage = function () { 
+  myConn.on('data', function (data) {
 
-var editor = null;
-
-async function SetUpFilesystem() {
-  window.fs = new LightningFS('fs')
-  window.pfs = window.fs.promises
-
-  window.dir = '/'
-  try {
-    await pfs.mkdir(dir);
-  } catch (e) {
-
-  }
-  // Behold - it is empty!
-  await pfs.readdir(dir);
-  await git.clone({
-    fs,
-    http,
-    dir,
-    corsProxy: 'https://cors.isomorphic-git.org',
-    url: 'https://github.com/SmolNoob7341/SmolNoob7341.github.io.git',
-    ref: 'SmolNoob',
-    singleBranch: true,
-    depth: 4
-  });
-
-  // Now it should not be empty...
-  return pfs;
-}
-
-function setUpEditor(data) {
-
-  require.config({ paths: { 'vs': 'https://unpkg.com/monaco-editor@latest/min/vs' } });
-  window.MonacoEnvironment = { getWorkerUrl: () => proxy };
-
-  let proxy = URL.createObjectURL(new Blob([`
-    self.MonacoEnvironment = {
-      baseUrl: 'https://unpkg.com/monaco-editor@latest/min/'
-    };
-    importScripts('https://unpkg.com/monaco-editor@latest/min/vs/base/worker/workerMain.js');
-  `], { type: 'text/javascript' }));
-
-  require(["vs/editor/editor.main"], function () {
-    editor = monaco.editor.create(document.getElementById('container'), {
-      value: [data
-      ].join('\n'),
-      language: 'javascript',
-      theme: 'vs-dark'
-    });
-  });
-
-}
-
-async function setUpExplorer(explorer, pfs, path) {
-  var ret1 = await pfs.readdir(path);
-  for (var el of ret1) {
-    var ret2 = await pfs.stat(path + el);
-    var entry = document.createElement('div');
-    entry.className = 'fileEntry';
-
-    let fp = path + el;
-    entry.innerHTML = fp;
-    entry.addEventListener("click", async function () {
-      console.log(fp)
-      var contents = await pfs.readFile(fp, { encoding: "utf8" })
-      var model = monaco.editor.createModel(contents);
-
-      editor.setModel(model);
-    });
-    explorer.appendChild(entry);
-    if (ret2.type == "dir") {
-      await setUpExplorer(explorer, pfs, path + el + "/");
+    if(data.type == 'sendMessage'){
+      if(previousMessageAuthor != 'him'){
+        addDivToBorder('him', peerUsername, '');
+        previousMessageAuthor = 'him';
+      }
+      addDivToBorder('messageIn', data.message, lastId); 
+      lastId += 1;
+    }else if(data.type == 'setUsername'){
+      peerUsername = data.username;
+    }else if(data.type == 'updateMessage'){
+      document.getElementById(data.msgId).innerHTML = data.text;
+    }else if(data.type == 'isTyping'){
+      usernameIsTyping = peerUsername+' is typing...';
+      document.getElementById('span').innerHTML = usernameIsTyping;
+    }else if(data.type == 'isntTyping'){
+      document.getElementById('span').innerHTML = '';
     }
+  })
+}
+var lastId = 0;
+var editingId = -1;
+var addDivToBorder = function (className, text, id) {
+  var iDiv = document.createElement('div');
+  iDiv.id = id;
+  iDiv.className = className;
+  iDiv.innerHTML = text;
+  var tmpLastId = lastId;
+  if(className == 'messageOut') {
+    iDiv.addEventListener("click", function() {
+      if(editingId != -1){
+       document.getElementById(editingId).style.color = 'blue';
+     }
+      editingId = tmpLastId;
+      document.getElementById("message").placeholder = "Editing text";
+      document.getElementById('messages').innerHTML = 'EDIT';
+      document.getElementById(editingId).style.color = "red";
+      msgInput.value = document.getElementById(editingId).innerHTML;
+  });
+}
+  document.getElementById('border').appendChild(iDiv);
+}
 
+var peerUsername = "Him";
+var msgInput = document.getElementById("message");
+var sendButton = document.getElementById("messages");
+var channelInput = document.getElementById('lname');
+var myConn = null;
+
+sendButton.disabled = true;
+msgInput.disabled = true;
+var previousMessageAuthor = "";
+
+var hostingButton = document.getElementById("hosting");
+var isHosting = function () {
+  console.log(channelInput.value, "is hosting");
+  var peer = new Peer(channelInput.value);
+  peer.on('connection', function (conn) {
+    sendButton.disabled = false;
+    msgInput.disabled = false;
+    myConn = conn;
+    myConn.on('open', function () {
+      myConn.send({ "type": "setUsername", "username": username.value });
+    }) 
+    handleMessage();
+
+  });
+};
+hostingButton.addEventListener("click", isHosting);
+
+var joiningButton = document.getElementById("joining");
+var isJoining = function () {
+  console.log(channelInput.value, "is joining");
+  var peer = new Peer();
+  peer.on("open", function () {
+    var conn = peer.connect(channelInput.value);
+    sendButton.disabled = false;
+    msgInput.disabled = false;
+    myConn = conn;
+    myConn.on('open', function () {
+      myConn.send({ "type": "setUsername", "username": username.value });
+      handleMessage();
+    })
+  });
+};
+joiningButton.addEventListener("click", isJoining);
+
+var isSending = function(){
+  document.getElementById("message").placeholder = "Enter text";
+  document.getElementById('messages').innerHTML = 'SEND';
+  if(editingId < 0){
+    if(previousMessageAuthor != "me"){
+      addDivToBorder("me", username.value, '');
+    }
+    myConn.send({'type': 'sendMessage', 'message': msgInput.value});
+    addDivToBorder("messageOut", msgInput.value, lastId);
+    lastId += 1;
+    msgInput.value = '';
+    previousMessageAuthor = "me";
+  }else{
+    myConn.send({'type': 'updateMessage', 'text': msgInput.value, 'msgId': editingId});
+    document.getElementById(editingId).
+    innerHTML = msgInput.value;
+    if(msgInput.value == ''){
+      document.getElementById(editingId).style.fontFamily = "cursive";
+      document.getElementById(editingId).innerHTML = 'Deleted Message';
+    }
+    msgInput.value = '';
+    editingId = -1;
   }
 }
+sendButton.addEventListener("click", isSending);
 
-async function setUpEverything() {
-  var pfs = await SetUpFilesystem()
-  setUpExplorer(document.getElementById("explorer"), pfs, "/");
-  setUpEditor("Click on a file")
+msgInput.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    if(editingId != -1){
+      document.getElementById(editingId).style.color = "blue";
+    }
+    event.preventDefault();
+    document.getElementById("messages").click();
+  }
+})
+function updateScroll(){
+    var element = document.getElementById("border");
+    element.scrollTop = element.scrollHeight;
 }
+setInterval(updateScroll,100);
 
-setUpEverything()
+
+var myFocus = function (){
+  usernameIsTyping = username.value+' is typing...';
+  myConn.send({'type': 'isTyping', 'message': usernameIsTyping});
+}
+var notFocus = function (){
+    myConn.send({'type': 'isntTyping', 'message': ''})
+    document.getElementById('span').innerHTML = '';
+}
+msgInput.addEventListener("keypress", myFocus);
+sendButton.addEventListener("click", notFocus);
+msgInput.addEventListener("blur", notFocus);
